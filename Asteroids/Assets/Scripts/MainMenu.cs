468fa8a3f7e1f98b312f4f4ae271c6112e9908bf
Emily Canto <56757680+dronade@users.noreply.github.com>
2023-02-09 17:18:41 +0000

    fixing scene order

--- Asteroids/Assets/Scripts/MainMenu.cs
+++ Asteroids/Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
    public void StartGame(){
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
    }
 
    public void QuitGame(){
